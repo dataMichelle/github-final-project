96bcf84b3c5000abb20fc462fb370d3a8ff9e0bc
Michelle <116389047+dataMichelle@users.noreply.github.com>
2024-06-24 13:39:40 -0500

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,14 @@
+Simple Interest Calculation:
+
+Simple Interest Equation (Principal + Interest)
+A = P(1 + rt)
+
+Where:
+
+A = Total Accrued Amount (principal + interest)
+P = Principal Amount
+I = Interest Amount
+r = Rate of Interest per year in decimal; r = R/100
+R = Rate of Interest per year as a percent; R = r * 100
+t = Time Period involved in months or years
+From the base formula, A = P(1 + rt) derived from A = P + I and since I = Prt then A = P + I becomes A = P + Prt which can be rewritten as A = P(1 + rt)
